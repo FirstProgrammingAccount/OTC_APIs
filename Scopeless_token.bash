872e304f9f49e00566ab1cc5cf3217f6bc70d36c
FirstProgrammingAccount <77907975+FirstProgrammingAccount@users.noreply.github.com>
2021-01-31 18:17:30 +0100

    Update Scopeless_token.bash

--- Scopeless_token.bash
+++ Scopeless_token.bash
@@ -1,4 +1,3 @@
-#!/bin/bash
-#necessary variables: username, password, domain_name, iam_endpoint
+#variables required: username, password, domain_name, iam_endpoint
 
 curl -i -k -H 'Content-Type:application/json;charset=utf8' -H 'Accept:application/json' -d '{"auth":{"identity":{"methods":["password"],"password":{"user":{"name":"'$username'","password":"'$password'","domain":{"name":"'$domain_name'"}}}}}}' -X POST https://$iam_endpoint/v3/auth/tokens
